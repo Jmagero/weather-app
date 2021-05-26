4ea1aa2c6c7e32d0c5b4ec4d1e2427e23260cc50
Jmagero <jmagero2017@gmail.com>
2021-05-26 18:33:20 +0300

    Add weather info

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ async function fetchData(country){
     .then(data => {
         showWeather(data)
     })
-    .catch(function() {
+    .catch(function(e) {
 		e
 	});
 }
@@ -26,6 +26,17 @@ async function fetchData(country){
 function showWeather(d){
     var celcius = Math.round(parseFloat(d.main.temp)-273.15);
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+    var name = d.sys.country;
+    var city = d.name;
+    var visibility = Math.round(d.visibility/1000);
+    var wind = d.wind.speed;
+    var humidity = d.main.humidity;
+    var description = d.weather[0].main;
 	
-	document.getElementById('temp').innerHTML = celcius;
+	document.getElementById('temp').innerHTML = 'temp: '+ celcius + '&deg';
+    document.getElementById('city').innerHTML = ' '+ city + ',' + name;
+    document.getElementById('visibility').innerText = "Visibility:" + visibility+'km';
+    document.getElementById('wind').innerText = "Wind Speed: " + wind + 'm/s';
+    document.getElementById('humidity').innerText = 'Humidity: ' + humidity + '%';
+    document.getElementById('description').innerText = description;
 }
