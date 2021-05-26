8715b3ec0965fbe99b9bb0dd0b4d34e32ef42020
Jmagero <jmagero2017@gmail.com>
2021-05-26 04:26:48 +0300

    Add function to get weather data

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 import '@fortawesome/fontawesome-free/js/all.js';
+async function fetchData(country){
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${api}`
+    let res = await fetch(url, {mode: "cors"}) 
+    let response = await res.json()
+    .then(data => {
+        showWeather(data)
+    })
+    .catch(function() {
+		e
+	});
+}
 
 async function loadData(name){
     const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=1045a35b8ebf5f8cfdcd94f356e5da65`, {mode: "cors"});
